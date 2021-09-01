7ea15e557631007af44968cb8cbd5945b90a1c51
Bas Broekhuizen <bas.broekhuizen@gmail.com>
2021-09-01 15:44:01 +0200

    Add cors headers to api

--- website/next.config.js
+++ website/next.config.js
@@ -16,4 +16,17 @@ module.exports = withBundleAnalyzer({
     domains: ['localhost', 'cms.onderzoek-en-statistiek.nl', 'acc.cms.onderzoek-en-statistiek.nl'],
   },
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js', 'api.js'],
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ]
+  },
 })
