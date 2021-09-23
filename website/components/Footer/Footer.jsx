4fc8d2f05386b569e4f008dc1da0b6012e259384
Aram Limpens <a.limpens@amsterdam.nl>
2021-09-23 14:03:31 +0000

    Implement restyled footer

--- website/components/Footer/Footer.jsx
+++ website/components/Footer/Footer.jsx
@@ -1,58 +1,101 @@
 import {
   CompactThemeProvider,
   Footer as FooterComponent,
-  FooterTop,
-  FooterSection,
-  FooterBottom,
   List,
   ListItem,
-  Link,
+  Link as LinkASC,
+  Heading,
   Paragraph,
 } from '@amsterdam/asc-ui'
+import { Phone, Email } from '@amsterdam/asc-assets'
 
+import Container from '../Container/Container'
+import LinkNext from '../Link/Link'
 import * as Styled from './Footer.style'
 
 const Footer = () => (
   <CompactThemeProvider>
     <FooterComponent>
-      <FooterTop>
-        <Styled.Container>
-          <FooterSection title="Colofon" ssr>
-            <List>
-              <ListItem>
-                <Link darkBackground href="/" inList>
-                  Over Onderzoek en Statistiek
-                </Link>
-              </ListItem>
-              <ListItem>
-                <Link darkBackground href="/" inList>
-                  Contact
-                </Link>
-              </ListItem>
-            </List>
-          </FooterSection>
-          <FooterSection title="Panels en enquêtes" ssr>
-            <Paragraph gutterBottom={8}>
-              Bent u uitgenodigd om mee te doen aan onderzoek
-              of wilt u lid worden van ons panel?
-            </Paragraph>
-            <List>
-              <ListItem>
-                <Link darkBackground href="https://www.amsterdam.nl/onderzoek/" inList>
-                  Meedoen aan onderzoek
-                </Link>
-              </ListItem>
-            </List>
-          </FooterSection>
-        </Styled.Container>
-      </FooterTop>
-      <Styled.BottomContainer>
-        <FooterBottom>
-          <Link href="/" inList>
-            Privacy and cookies
-          </Link>
-        </FooterBottom>
-      </Styled.BottomContainer>
+      <Styled.Container>
+        <Styled.Section>
+          <Heading styleAs="h2" color="bright" gutterBottom={28}>Contact</Heading>
+          <Paragraph>
+            Heeft u een vraag en kunt u het antwoord niet vinden op deze website?
+            Neem dan contact met ons op.
+          </Paragraph>
+          <List>
+            <ListItem>
+              <LinkASC darkBackground href="mailto:redactie.ois@amsterdam.nl">
+                <Styled.Icon size={20}>
+                  <Email />
+                </Styled.Icon>
+                <span>E-mail</span>
+              </LinkASC>
+            </ListItem>
+            <ListItem>
+              <LinkASC darkBackground href="tel:+31202510333">
+                <Styled.Icon size={20}>
+                  <Phone />
+                </Styled.Icon>
+                <span>020 251 0333</span>
+              </LinkASC>
+            </ListItem>
+          </List>
+        </Styled.Section>
+        <Styled.Section>
+          <Heading styleAs="h2" color="bright" gutterBottom={28}>Panels en enquêtes</Heading>
+          <Paragraph gutterBottom={16}>
+            Bent u uitgenodigd om mee te doen aan onderzoek
+            of wilt u lid worden van ons panel?
+          </Paragraph>
+          <List>
+            <ListItem>
+              <LinkASC darkBackground href="https://www.amsterdam.nl/onderzoek/" inList>
+                Meedoen aan onderzoek
+              </LinkASC>
+            </ListItem>
+          </List>
+        </Styled.Section>
+        <Styled.Section>
+          <Heading styleAs="h2" color="bright" gutterBottom={28}>Onderzoek en Statistiek</Heading>
+          <List>
+            <ListItem>
+              <LinkNext darkBackground href="/artikel/over-onderzoek-en-statistiek" inList>
+                Over Onderzoek en Statistiek
+              </LinkNext>
+            </ListItem>
+            <ListItem>
+              <LinkNext darkBackground href="/artikel/veelgestelde-vragen" inList>
+                Veelgestelde vragen
+              </LinkNext>
+            </ListItem>
+            <ListItem>
+              <LinkASC darkBackground href="https://www.amsterdam.nl/nieuwsbrieven/bestuur-organisatie/dienstverlening/nieuwsbrief-data-informatie/nieuwsbrief-data-informatie/" inList>
+                Nieuwsbrief
+              </LinkASC>
+            </ListItem>
+            <ListItem>
+              <LinkASC darkBackground href="https://www.amsterdam.nl/bestuur-organisatie/werkenbij/externe/?zoeken=true&zoeken_term=&selectie_zoeken_op_maanden=AllYears&zoek_clustered_keywords_cluster=3669_3670_3671_3672_3673_3674_3675_3676_3677_3678_3679_3717_3681_3682_3683_3684_3685_3686_3688&zoek_clustered_keywords=3717&zoek_clustered_keywords_cluster=3662_3664_3665_3666_3667_3668_3715&zoek_clustered_keywords_cluster=3689_3690_3692_3693" inList>
+                Vacatures
+              </LinkASC>
+            </ListItem>
+          </List>
+        </Styled.Section>
+      </Styled.Container>
+      <Container verticalPadding={0}>
+        <Styled.List>
+          <Styled.ListItem>
+            <LinkASC href="https://www.amsterdam.nl/privacy/">
+              Privacy
+            </LinkASC>
+          </Styled.ListItem>
+          <Styled.ListItem>
+            <LinkNext href="/artikel/toegankelijkheidsverklaring">
+              Toegankelijkheid
+            </LinkNext>
+          </Styled.ListItem>
+        </Styled.List>
+      </Container>
     </FooterComponent>
   </CompactThemeProvider>
 )
--- website/components/Footer/Footer.style.js
+++ website/components/Footer/Footer.style.js
@@ -1,17 +1,40 @@
-import { breakpoint } from '@amsterdam/asc-ui'
+import {
+  breakpoint,
+  themeColor,
+  Icon as IconASC,
+} from '@amsterdam/asc-ui'
 import styled from 'styled-components'
 
 import ContainerComponent from '../Container/Container'
 
 export const Container = styled(ContainerComponent)`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 32px;
+  padding-top: 32px;
+  padding-bottom: 48px;
+  background-color: ${themeColor('primary')};
+  color: white;
 
-  @media screen and ${breakpoint('max-width', 'tabletM')} {
+  @media screen and ${breakpoint('max-width', 'laptop')} {
     grid-template-columns: auto;
   }
 `
 
-export const BottomContainer = styled.div`
-  margin: 0 auto;
+export const Section = styled.div`
+  max-width: 440px;
+`
+
+export const Icon = styled(IconASC)`
+  margin-right: 12px;
+`
+
+export const List = styled.ul`
+  padding: 12px 0;
+  margin: 0;
+`
+
+export const ListItem = styled.li`
+  display: inline;
+  margin-right: 40px;
 `
